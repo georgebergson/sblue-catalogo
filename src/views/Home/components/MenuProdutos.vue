a4349fd9a67d11843fe3ad02921f78ba3092c6b6
George Bergson <george_bergson@hotmail.com>
2023-11-04 15:41:00 -0300

    Efeito Ancora

--- src/views/Home/components/MenuProdutos.vue
+++ src/views/Home/components/MenuProdutos.vue
@@ -1,52 +1,90 @@
 <template>
-    <section id="menuancora" class="px-2 mt-3 nav flex space-x-2 overflow-x-auto ">
-        <button @click="menuAncora('#pizzas')" class="pizzas whitespace-nowrap text-blue-700 border-2 border-blue-700 hover:bg-blue-800 hover:text-white focus:outbuttonne-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
+    <section id="menuancora" class="bg-white w-full px-2 pt-3 nav flex space-x-2 overflow-x-auto ">
+        <button @click="menuAncora('#pizzas')"
+            class="pizzas whitespace-nowrap text-blue-700 border-2 border-blue-700 focus:outbuttonne-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
             PIZZAS
         </button>
-        <button @click="menuAncora('#sanduiches')" class="sanduiches whitespace-nowrap text-blue-700 border-2 border-blue-700 hover:bg-blue-800 hover:text-white focus:outbuttonne-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
+        <button @click="menuAncora('#sanduiches')"
+            class="sanduiches whitespace-nowrap text-blue-700 border-2 border-blue-700 focus:outbuttonne-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
             SANDUICHES
         </button>
-        <button @click="menuAncora('#tapioca-recheada')" class="tapioca-recheada whitespace-nowrap text-blue-700 border-2 border-blue-700 hover:bg-blue-800 hover:text-white focus:outbuttonne-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
+        <button @click="menuAncora('#tapioca-recheada')"
+            class="tapioca-recheada whitespace-nowrap text-blue-700 border-2 border-blue-700 focus:outbuttonne-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
             TAPIOCA RECHEADA
         </button>
-        <button @click="menuAncora('#bebidas-com-gelo')" class="bebidas-com-gelo whitespace-nowrap text-blue-700 border-2 border-blue-700 hover:bg-blue-800 hover:text-white focus:outbuttonne-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
-          BEBIDAS COM GELO
+        <button @click="menuAncora('#bebidas-com-gelo')"
+            class="bebidas-com-gelo whitespace-nowrap text-blue-700 border-2 border-blue-700 focus:outbuttonne-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            BEBIDAS COM GELO
         </button>
     </section>
 </template>
 
 <script setup lang="ts">
 
-function menuAncora(value: any){
+function menuAncora(value: any) {
     location.href = value;
     menuAncoraColor(value);
 }
 
-function menuAncoraColor(value: any){
-    var menuancora = document.getElementById("menuancora");
-    var botao = menuancora?.children;
+function menuAncoraColor(value: any) {
+    const menuancora = document.getElementById("menuancora");
+    const botao = menuancora?.children;
 
-    if(botao){
+    if (botao) {
         for (let index = 0; index < botao.length; index++) {
-        var botaoItem = botao[index];
-        
-       if("#"+botaoItem.classList[0] == value){
-           botao[index].classList.remove("text-blue-700");
-           botao[index].classList.add("text-white");
-           botao[index].classList.add("bg-blue-700");
-       }else{
-        botao[index].classList.add("text-blue-700");
-           botao[index].classList.remove("text-white");
-           botao[index].classList.remove("bg-blue-700");
-       }
-        
-    }
+            const botaoItem = botao[index];
+
+            if ("#" + botaoItem.classList[0] == value) {
+                botao[index].classList.remove("text-blue-700");
+                botao[index].classList.add("text-white");
+                botao[index].classList.add("bg-blue-700");
+            } else {
+                console.log(botaoItem.classList[0])
+                botao[index].classList.remove("text-white");
+                botao[index].classList.remove("bg-blue-700");
+                botao[index].classList.add("text-blue-700");
+            }
+
+        }
     }
 }
+
+function ancoraLateral(posicaoDoScroll: any){
+    const categoria = document.getElementsByClassName("categoria");
+    console.log(posicaoDoScroll)
+    if (categoria) {
+        const posicaoY = Math.floor(posicaoDoScroll);
+        for (let index = 0; index < categoria.length; index++) {
+            const botaoItemTop = categoria[index].offsetTop;
+           const botaoItemTopHeight = categoria[index].offsetHeight;
+           if( (posicaoY >= botaoItemTop) && (posicaoY <= botaoItemTop+botaoItemTopHeight) ){
+            menuAncoraColor("#"+categoria[index].children[0].children[0].id);
+           }
+           
+        }
+    }
+}
+
+function menuTopo(posicaoDoScroll: any){
+    const posicaoY = Math.floor(posicaoDoScroll);
+    const menuancora = document.querySelectorAll("#menuancora");
+    if(posicaoY >= 283){
+        menuancora[0].classList.add('fixed');
+        menuancora[0].classList.add('top-0');
+    }else{
+        menuancora[0].classList.remove('fixed');
+        menuancora[0].classList.remove('top-0');
+    }
+}
+window.addEventListener('scroll', function () {
+    const posicaoDoScroll = window.scrollY;
+    //console.log('Posição do scroll:', posicaoDoScroll);
+    ancoraLateral(posicaoDoScroll);
+    menuTopo(posicaoDoScroll);
+});
 </script>
 
 <style>
- html {
+html {
     scroll-behavior: smooth;
-  }
-</style>
+}</style>
